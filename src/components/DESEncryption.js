ad4a06fedf8eab56957c561d6e8581d10a902efa
TianyuWu <tianyu.wu1@ucdconnect.ie>
2023-10-02 18:58:36 +0800

    Initial commit

---
+++ src/components/DESEncryption.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import { Button, Input, Upload, message } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
+import { stringToBinary, binaryToString } from '../utils/binary';
+
+function DESEncryption() {
+    const [key, setKey] = useState("");       // To store the key
+    const [text, setText] = useState("");     // To store the plaintext
+    const [cipherText, setCipherText] = useState('');
+
+    // File upload handler
+    const handleFileUpload = (info) => {
+        if (info.file.status === 'done' || info.file.status === 'error') {
+            return; // Do nothing if upload status is 'done' or 'error'
+        }
+
+        const reader = new FileReader();
+        reader.onload = () => {
+            setText(reader.result);
+        };
+        reader.onerror = error => console.log(error.target.error.code);  // This will help identify any FileReader errors
+
+        if (info.file instanceof Blob) {
+            reader.readAsText(info.file);
+        } else {
+            console.error("Couldn't find the origin file object.");
+        }
+    };
+
+    const handleEncryption = () => {
+        const binary = stringToBinary(text);
+        message.info('Implement encryption')
+        setCipherText(binary);
+        return binary;
+    };
+
+    return (
+        <div>
+            <h1>DES Encryption & Decryption Tool</h1>
+
+            <Input
+                placeholder="Enter key"
+                value={key}
+                onChange={e => setKey(e.target.value)}
+                style={{ marginBottom: 20 }}
+            />
+
+            <Input.TextArea
+                placeholder="Enter plaintext here"
+                value={text}
+                onChange={e => setText(e.target.value)}
+                rows={4}
+                style={{ marginTop: 20, marginBottom: 20 }}
+            />
+
+            <Upload beforeUpload={() => false} onChange={handleFileUpload} showUploadList={false}>
+                <Button icon={<UploadOutlined />}>Upload plaintext or ciphertext file</Button>
+            </Upload>
+
+            <Input.TextArea
+                placeholder="Encrypted text will appear here..."
+                value={cipherText}
+                rows={4}
+                onChange={e => setCipherText(e.target.value)}
+                style={{ marginTop: 20, marginBottom: 20 }}
+            />
+
+            <Button type='primary' onClick={handleEncryption} style={{ marginRight: 20 }}>
+                Encrypt
+            </Button>
+            <Button type='primary' onClick={() => message.info('Implement decryption')}>
+                Decrypt
+            </Button>
+        </div>
+    );
+}
+
+export default DESEncryption;
---
+++ src/utils/binary.js
@@ -0,0 +1,16 @@
+export function stringToBinary(input) {
+    const characters = input.split(''); // Split string into array of characters
+    return characters.map(char => {
+        const binary = char.charCodeAt(0).toString(2); // Convert character to binary
+        const paddedBinary = binary.padStart(8, '0');
+        return paddedBinary;
+    }).join(''); // Join all the binary values together
+}
+
+export function binaryToString(binary) {
+    const bytes = binary.match(/.{1,8}/g).join(' '); // Split the binary string into bytes of 8
+    const chars = bytes.split(' ').map(byte => {
+        return String.fromCharCode(parseInt(byte, 2));
+    });
+    return chars.join(''); // Join the characters together
+}
