d3398e3f7c510aaba4283c1ac2281267a3c38a24
TianyuWu <tianyu.wu1@ucdconnect.ie>
2023-11-09 19:53:57 +0800

    feat: update rsa

--- src/rsautils/keyGen.js
+++ src/rsautils/keyGen.js
@@ -1,8 +1,11 @@
 // rsautils/keyGen.js
+import { gcd } from 'math';
+const { generatePrime } = require('crypto');
 
-const generatePrimeNum = () => {
+const generatePrimeNum = (bits) => {
     // TODO: Implement prime number generation
-    // Consider using an existing library or implementing the Rabin-Miller primality test
+    // Consider using an existing library
+    return generatePrime(bits);
 };
 
 const calculateN = (p, q) => p * q;
@@ -12,16 +15,28 @@ const calculateEulerTotient = (p, q) => (p - 1) * (q - 1);
 const selectE = (totient) => {
     // TODO: Implement selection of e that is co-prime to totient
     // This might involve checking gcd(e, totient) === 1
+    for (let i = 2; i < totient; i++) {
+        if (gcd(i, totient) === 1) {
+            return i;
+        }
+    }
+    throw new Error('Failed to select e');
 };
 
 const calculateD = (e, totient) => {
     // TODO: Implement calculation of d
     // This involves finding an integer d such that (d * e) % totient === 1
+    for (let i = 1; i < totient; i++) {
+        if ((i * e) % totient === 1) {
+            return i;
+        }
+    }
+    throw new Error('Failed to calculate d');
 };
 
 const generateKeys = () => {
-    const p = generatePrimeNum();
-    const q = generatePrimeNum();
+    const p = generatePrimeNum(1024);
+    const q = generatePrimeNum(1024);
     const n = calculateN(p, q);
     const totient = calculateEulerTotient(p, q);
     const e = selectE(totient);
